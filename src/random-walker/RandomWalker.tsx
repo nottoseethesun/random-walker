ad6a022eb8c3bbd9b14651deb16d41fffe666da2
christopherbalz <christopherbalz@yahoo.com>
2018-09-25 22:58:22 -0700

    feat: now has contrasting colors for the robots

--- src/random-walker/RandomWalker.tsx
+++ src/random-walker/RandomWalker.tsx
@@ -1,6 +1,11 @@
 import * as React from 'react';
 import * as WS from 'websocket';
+import ColorScheme from 'color-scheme';
 import './RandomWalker.css';
+
+const START_HUE = 178; // A green.
+const COLOR_SCHEME = 'contrast';
+const COLOR_VARIATION = 'hard';
 
 type Props = {
   numRobots: number;
@@ -10,7 +15,7 @@ type Props = {
 
 type State = {
   clearInterval: number;
-  numRobots: number;
+  colors: string[];
   ws: WS.w3cwebsocket;
 };
 
@@ -20,7 +25,7 @@ class RandomWalker extends React.Component<Props, State> {
     const { numRobots, refreshRateMS, url } = props;
     this.state = {
       clearInterval: -1,
-      numRobots,
+      colors: this.getColors(),
       ws: new WS.w3cwebsocket(`${url}?interval=${refreshRateMS}&num_robots=${numRobots}`),
     };
     const title = 'random-walker ws client';  // @to-do: move the user-facing messages out to user-text file
@@ -47,9 +52,27 @@ class RandomWalker extends React.Component<Props, State> {
   public render() {
     return (
       <div className="frbtcs-random-walker">
-      {this.state.numRobots}
+      {this.props.numRobots}
       </div>
     );
+  }
+
+  private getColors(): string[] {
+    const cs = new ColorScheme;
+    let colors: string[] = [];
+    const hues: number[] = [];
+    const COLORS_PER_HUE= 8;
+    const HUE_DEGREES = 360;
+    const numColorSets = Math.ceil(this.props.numRobots / COLORS_PER_HUE);
+    const hueStep = Math.round(HUE_DEGREES / numColorSets);
+    for(let i = 0; i < numColorSets; i++) {
+      hues.push(START_HUE + (i * hueStep));
+    }
+    hues.forEach( (hue: number) => {
+      const result = cs.from_hue(hue).scheme(COLOR_SCHEME).variation(COLOR_VARIATION);
+      colors = colors.concat(result.colors());
+    });
+    return colors;
   }
 
   private updateState(event: { data: {}; }) {
